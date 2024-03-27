8ce46c571040c592168a85a2240728b9d4470eca
lukagoderdzishvili <goderdzishvili21@gmail.com>
2024-03-27 14:47:39 +0400

    add write-comment component

--- src/app/components/comment/comment.component.ts
+++ src/app/components/comment/comment.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommentFormComponent } from '../comment-form/comment-form.component';
 
 @Component({
   selector: 'app-comment',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, CommentFormComponent],
   templateUrl: './comment.component.html',
   styleUrl: './comment.component.scss'
 })
